fffeb6651b2b96c0fc6ce59d8e35b126b73d0293
Bronson Zgeb <bronson.zgeb@unity3d.com>
2021-08-16 22:01:49 -0400

    Added return to previous scene on exit option

--- Assets/SceneSelectorTool/Editor/SceneSelectorSettings.cs
+++ Assets/SceneSelectorTool/Editor/SceneSelectorSettings.cs
@@ -1,14 +1,14 @@
 using System;
 using UnityEditor;
-using UnityEngine;
 
 [FilePath("Assets/SceneSelectorTool/Editor/SceneSelectorSettings.asset", FilePathAttribute.Location.ProjectFolder)]
-[CreateAssetMenu]
 public class SceneSelectorSettings : ScriptableSingleton<SceneSelectorSettings>
 {
     public bool PlayOnOpen;
-
-    void OnDisable()
+    public bool ReturnOnExit;
+    public string PreviousScenePath;
+    
+    void OnDestroy()
     {
         Save(true);
     }
--- Assets/SceneSelectorTool/Editor/SceneSelectorWindow.cs
+++ Assets/SceneSelectorTool/Editor/SceneSelectorWindow.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class SceneSelectorWindow : EditorWindow
@@ -8,6 +9,12 @@ public class SceneSelectorWindow : EditorWindow
     static void OpenWindow()
     {
         GetWindow<SceneSelectorWindow>();
+    }
+
+    [InitializeOnLoadMethodAttribute]
+    static void RegisterCallbacks()
+    {
+        EditorApplication.playModeStateChanged += ReturnToPreviousScene;
     }
 
     void CreateGUI()
@@ -19,20 +26,40 @@ public class SceneSelectorWindow : EditorWindow
         {
             SceneSelectorSettings.instance.PlayOnOpen = evt.newValue;
         });
+        var returnOnExitToggle = new Toggle("Return on Exit");
+        returnOnExitToggle.SetValueWithoutNotify(SceneSelectorSettings.instance.ReturnOnExit);
+        returnOnExitToggle.RegisterValueChangedCallback(evt =>
+        {
+            SceneSelectorSettings.instance.ReturnOnExit = evt.newValue;
+        });
         rootVisualElement.Add(playOnOpenToggle);
+        rootVisualElement.Add(returnOnExitToggle);
         foreach (var sceneGuid in sceneGuids)
         {
             rootVisualElement.Add(CreateSceneButton(sceneGuid));
         }
     }
 
+    static void ReturnToPreviousScene(PlayModeStateChange change)
+    {
+        if (SceneSelectorSettings.instance.PlayOnOpen && change == PlayModeStateChange.EnteredEditMode)
+        {
+            EditorSceneManager.OpenScene(SceneSelectorSettings.instance.PreviousScenePath, OpenSceneMode.Single);
+        }
+    }
+
     Button CreateSceneButton(string sceneGuid)
     {
         var scenePath = AssetDatabase.GUIDToAssetPath(sceneGuid);
-        var button = new Button(() =>
+        var button = new Button(async () =>
         {
+            SceneSelectorSettings.instance.PreviousScenePath = SceneManager.GetActiveScene().path;
+
             EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
-            EditorApplication.isPlaying = SceneSelectorSettings.instance.PlayOnOpen;
+            if (SceneSelectorSettings.instance.PlayOnOpen)
+            {
+                EditorApplication.EnterPlaymode();
+            }
         });
         var sceneAsset = AssetDatabase.LoadAssetAtPath(scenePath, typeof(SceneAsset));
         button.text = $"{sceneAsset.name}";
